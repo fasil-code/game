70c4b6d68165d1f38b5b5d6572629b864b8dad5c
Jameel Kaisar <65322473+JameelKaisar@users.noreply.github.com>
2021-08-04 17:40:33 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,55 +1,42 @@
+function getRandom() {
+  return Math.floor(Math.random()*3)+1;
+}
 
-let b=document.getElementById("sec").value;
-let c=document.getElementById("thi").value;
+function checkResult(result) {
+  if (result) {
+        document.querySelector("h1").innerHTML="Sucess !";        
+        document.querySelector("h1").style.color="blue";        
+        document.querySelector("body").style.backgroundColor="green";   
+  }
+  else {
+        document.querySelector("h1").innerHTML="Failed !";
+        document.querySelector("body").style.backgroundColor="red"; 
+   }
+}
 
-var x=Math.floor(Math.random()*3)+1;
+var x = getRandom();
 
 document.getElementById("fir").addEventListener("click",function(){
    
-    let a=document.getElementById("fir").value;
-    
-    if(x==a){
-        document.querySelector("h1").innerHTML="Sucess !";        
-        document.querySelector("h1").style.color="blue";        
-        document.querySelector("body").style.backgroundColor="green";        
-
-    }
-    else{
-        document.querySelector("h1").innerHTML="Failed !";
-        document.querySelector("body").style.backgroundColor="red"; 
-    }
+   let a=document.getElementById("fir").value;
+   checkResult(a==x);
+   x = getRandom();
 
 });
 
 document.getElementById("sec").addEventListener("click",function(){
    
-    let a=document.getElementById("sec").value;
-
-if(x==a){
-    document.querySelector("h1").innerHTML="Sucess !";        
-    document.querySelector("h1").style.color="blue";        
-    document.querySelector("body").style.backgroundColor="green";        
-
-}
-else{
-    document.querySelector("h1").innerHTML="Failed !";
-    document.querySelector("body").style.backgroundColor="red"; 
-}
+   let a=document.getElementById("sec").value;
+   checkResult(a==x);
+   x = getRandom();
 
 });
+
 document.getElementById("thi").addEventListener("click",function(){
    
-    let a=document.getElementById("thi").value;
+   let a=document.getElementById("thi").value;
+   checkResult(a==x);
+   x = getRandom();
 
-if(x==a){
-    document.querySelector("h1").innerHTML="Sucess !";        
-    document.querySelector("h1").style.color="blue";        
-    document.querySelector("body").style.backgroundColor="green";        
-
-}
-else{
-    document.querySelector("h1").innerHTML="Failed !";
-    document.querySelector("body").style.backgroundColor="red"; 
-}
 });
 
